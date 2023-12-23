0fb8bcfa3713485bb4c218958cdcad6fa4935bf1
Tacos4brekky2 <123289331+Tacos4brekky2@users.noreply.github.com>
2023-12-23 14:07:03 -0800

    formatting and comments

--- Class/Board.py
+++ Class/Board.py
@@ -88,54 +88,38 @@ class Board():
         self.en_passant_target = (0, 0) if self.position[10] == '-' else (self.file_letters[self.position[10][0]], 8 - int(self.position[10][1]))
         self.populateBoard()
         self.startTurn(self.active_color)
-    
 
-    """
-    --- Generates the initial board position on start ---
-    ** Single-use
-    (Modifies initial empty board array in place)
 
-    Algorithm:
-        - Takes a converted FEN string as input *(self.position): list.
-        - Iterates through the ranks from 1-8,
-            through the squares in each rank from a-h.
-        - Places the integer equivalents of the pieces on
-            their respective squares. -> Positive integers for white, negative integers for black.
-        - If a number is found that represents a series of empty squares,
-            perform an equivalent number of loops to mark each square as
-            empty.
+    def populateBoard(
+            self
+    ) -> None:
+        """ Generates the initial board position on start
+    ** Single-use, modifies board array in place
 
     TODO: Only populates empty ranks (8)
           Can't understand something like 4PPPP
     """
-    def populateBoard(self):
-        for r in range(7, -1, -1):
-            for i, f in enumerate(self.position[r]):
-                if re.match('[1-8]', f):
-                    for n in range(int(f), int(self.position[r][int(i)])):
-                        self.board[r][n] = 0
-                elif re.match('[PBNRKQ]', f):
-                    self.board[r][i] = self.piece_numbers[f.lower()]
-                elif re.match('[pbnrkq]', f):
-                    self.board[r][i] = -self.piece_numbers[f.lower()]
+        self.function_count['board.populateBoard'] += 1
+
+        for rank in range(7, -1, -1):
+            for file, piece in enumerate(self.position[rank]):
+                if re.match('[1-8]', piece):
+                    for n in range(int(piece), int(self.position[rank][int(file)])):
+                        self.board[rank][n] = 0
+                elif re.match('[PBNRKQ]', piece):
+                    self.board[rank][file] = self.piece_numbers[piece.lower()]
+                elif re.match('[pbnrkq]', piece):
+                    self.board[rank][file] = -self.piece_numbers[piece.lower()]
 
  
-    """
-    --- Executes a move ---
-    ** MAIN FUNCTION
-
-    Functionality:
-        - Moves a piece to its destination square.
-        - Marks the square previously occupied by said piece as empty.
-        - Resets half move clock if no capture has been made.  Increments half move clock otherwise.
-    """
-
-
     def movePiece(
             self,
             move: tuple
     ) -> None:
+        """ Attempts to execute a player's move.
+    """
         self.function_count['board.movePiece'] += 1
+
         king_x = self.king_pos[st.PLAYER_WHITE][1] if move[0] == st.PLAYER_WHITE else self.king_pos[st.PLAYER_BLACK][1]
         king_y = self.king_pos[st.PLAYER_WHITE][0] if move[0] == st.PLAYER_WHITE else self.king_pos[st.PLAYER_BLACK][0]
         tmp = self.board[move[4]][move[5]]
@@ -161,38 +145,29 @@ class Board():
                 self.in_check[move[0] * -1] = 1
             self.incrementFiftyMoveCounter(tmp)
             self.startTurn(move[0] * -1)
-                # print(f'''
-# ===== Board.movePiece() =====
-# WHITE KING POS (Ri, Fi): {self.king_pos[0]}
-# BLACK KING POS (Ri, Fi): {self.king_pos[1]}
-# TURN: {self.active_color[1][move[0]]}
-# MOVE: {move}
-# ===========================
-#             ''')
 
-
-    """
-    *** OUTPUTS NEW MOVE TUPLE FORMAT ***
-    --- Converts initial and target move squares to a move tuple ---
-    Input:
-        - initial = [initial rank index, initial file index]
-        - target = [target rank index, target file index]
     
-    Output -> move(tuple(int))
+    def indexToMove(
+            self,
+            initial: list,
+            target: list
+    ) -> tuple:
+        """ Generates a move tuple from rank file index pairs
+    Input:
+    - initial = [initial rank index, initial file index]
+    - target = [target rank index, target file index]
+
+    Output: move
         Index   Meaning
-        0       white/black (0/1)
+        0       white/black (1/-1)
         1       piece number
         2       initial rank
         3       initial file
         4       target rank
         5       target file
     """
-    def indexToMove(self,
-                    initial: list,
-                    target: list
-                    ) -> tuple:
-        
         self.function_count['board.indexToMove'] += 1
+
         return (
             self.active_color, 
             self.board[initial[0]][initial[1]],
@@ -207,9 +182,10 @@ class Board():
             self,
             color: int
     ) -> list:
-        """ Returns all of a player's legal move tuples.
+        """ Generates a list of legal moves for a given player.
     """
         self.function_count['board.getLegalMoves'] += 1
+
         legal_moves = list()
         for r, rank in enumerate(self.board):
             for f, piece in enumerate(rank):
@@ -237,14 +213,18 @@ class Board():
                                 self.board[move[2]][move[3]] = tmp_origin
                                 self.king_pos[color] = tmp_king_origin
                                 legal_moves.append(move)
-        print(legal_moves)
         return legal_moves
     
     
 
-    def isValidMove(self,
-                    move: tuple) -> bool:
+    def isValidMove(
+            self,
+            move: tuple
+    ) -> bool:
+        """ Basic checks for invalid moves.
+    """
         self.function_count['board.isValidMove'] += 1
+
         target_square = self.board[move[4]][move[5]]
         # Trying to capture a friendly piece.
         if (
@@ -266,14 +246,13 @@ class Board():
         return False
     
 
-    def willNotCollide(self,
-                        move: tuple,
-                        vectors: list,
-                        offset: tuple
+    def willNotCollide(
+        self,
+        move: tuple,
+        vectors: list,
+        offset: tuple
     ) -> bool:
-        """ isValidMove() helper function.
-
-    Returns True if no collisions are detected.
+        """ Returns True if no collisions are detected.
 
     Pieces checked:
         - Pawn
@@ -281,8 +260,8 @@ class Board():
         - Rook
         - Queen
     """
-        
         self.function_count['board.willNotCollide'] += 1
+
         if move[1] == 3:
             return True
         path = []
@@ -318,6 +297,7 @@ class Board():
         """ Returns true if an offset moves a piece within the board
     """
         self.function_count['board.isValidOffset'] += 1
+
         if (
             (0 <= (piece_rank - offset[1]) <= 7) and
             (0 <= (piece_file + offset[0]) <= 7)
@@ -326,17 +306,19 @@ class Board():
         return False
 
     
-    def checkScan(self,
-                  color: int,
-                  king_x: list,
-                  king_y: list
+    def checkScan(
+        self,
+        color: int,
+        king_x: list,
+        king_y: list
     ) -> list:
-        """ Scans for opponent's pieces using their respective movement vectors.
+        """ Generates a list of pieces that are checking a given king.
 
         Starts from king and moves out.
         Iterates over each vector individually.
     """
         self.function_count['board.checkScan'] += 1
+
         diagonal = [[]] * 4
         straight = [[]] * 4
         knight = list()
@@ -363,20 +345,12 @@ class Board():
                 (self.isValidOffset(king_y, king_x, offset))
             ):
                 knight.append(offset)
-#         print(f'''
-# ===== Board.checkScan() =====
-# {self.active_color[1][color]} CHECK LIST
-# DIAGONAL: {diagonal}
-# STRAIGHT: {straight}
-# KNIGHT: {knight}
-# ''')
+
         checked_by = []
         for offset in knight:
-            #print(offset)
             target_square = self.board[king_y - offset[1]][king_x + offset[0]]
             rf_index = ((king_y - offset[1]), (king_x + offset[0]))
             if target_square == (-3 if color == st.PLAYER_WHITE else 3):
-                #print("KNIGHT")
                 checked_by.append(rf_index)
 
         for vector in straight:
@@ -391,14 +365,12 @@ class Board():
                         -6 if color == st.PLAYER_WHITE else 6
                     ])
                 ):
-                    #print("KING")
                     checked_by.append(rf_index)
                     
                 if target_square in [
                     -4 if color == st.PLAYER_WHITE else 4,
                     -5 if color == st.PLAYER_WHITE else 5
                 ]:
-                    #print("RQ")
                     checked_by.append(rf_index)
                 elif target_square != 0:
                     break
@@ -416,26 +388,23 @@ class Board():
                         -1 if color == st.PLAYER_WHITE else 1
                     ])
                 ):
-                    #print("KP")
                     checked_by.append(rf_index)
                 elif target_square in [
                     -2 if color == st.PLAYER_WHITE else 2, 
                     -5 if color == st.PLAYER_WHITE else 5
                     ]:
-                    #print('BQ')
                     checked_by.append(rf_index)
                 elif target_square != 0:
                     break
-        #print(f'CHECK LIIIIIIST: {checked_by}')
         return checked_by
-        
-        #print('==================')
     
     
     def startTurn(
             self,
             color: int
     ) -> None:
+        """ Switches whose turn it is
+    """
         os.system('clear')
         print(f'FUNCTION COUNT: {self.function_count}')
         self.function_count = defaultdict(int)
@@ -456,74 +425,14 @@ class Board():
     def incrementFiftyMoveCounter(
             self,
             target_piece: int
-        ) -> None:
+    ) -> None:
+        """ Increments fifty move counter if no piece was captured.
+    """
         self.function_count['board.incrementFiftyMoveCounter'] += 1
         if target_piece != 0:
             self.fifty_move_count = 0
         else:
             self.fifty_move_count += 1
-
-    """
-    *** NOT IN USE ***
-    *** OUTPUTS OLD MOVE TUPLE FORMAT ***
-
-    --- Standardizes algebraic notation inputs for use in the program ---
-    ** Base function
-
-    Algorithm:
-    - Takes an input string *(move_str) in the form of algebraic notation.
-    - Removes 'x' from capturing moves, adds 'p' to pawn moves.
-    - Determines the type of move using regular expressions.
-    - Returns a standardized tuple containing the integer equivalents
-        of the data provided in (move_str).
-        - If (move_str) is invalid, returns an empty tuple.
-
-    Functionality:
-        - All basic piece moves.
-        - Ambiguous moves.
-        - Resignation and draw offers.
-        - Castling.
-        - Invalid or empty move strings.
-
-    Output -> move(tuple(int)):
-        Index   Meaning
-        0       white/black (0/1) 
-        1       piece 
-        2       file 
-        3       rank 
-        4       disambiguation (rank) / pawn capture
-        5       disambiguation (file)
-
-    Examples:
-    e4 -> pe4 -> (x, 0, 4, 4, 69, 69)
-    nxf6 -> nf6 -> (x, 2, 5, 2, 0)
-
-    """
-    def moveStrConvert(self,
-                       move_str: str) -> tuple:
-        move = move_str.lower().replace('x', '')
-        if re.match('[a-h][1-8]', move):
-            move = 'p' + move
-        if re.match('[pbnrkq][a-h][1-8]', move):
-            return (self.active_color[0], self.piece_numbers[move[:1]], self.file_letters[move[1:2]], 8 - int(move[2:3]), 69, 69, 0)
-        elif re.match('[a-h][a-h][1-8]', move):
-            return (self.active_color[0], 1, self.file_letters[move[1:2]], 8 - int(move[2:3]), 69, self.file_letters[move[:1]], 11)
-        elif re.match('[a-h][1-8][a-h][1-8]', move):
-            return (self.active_color[0], self.piece_numbers[move[:1]], self.file_letters[move[2:3]], 8 - int(move[3:4]), 8 - int(move[1:2]), 69, 0)
-        elif re.match('[a-h][a-h][a-h][1-8]', move):
-            return (self.active_color[0], self.piece_numbers[move[:1]], self.file_letters[move[2:3]], 8 - int(move[3:4]), 69, self.file_letters[move[1:2]], 0)
-        elif re.match('[a-h][a-h][1-8][a-h][1-8]', move):
-            return (self.active_color[0], self.piece_numbers[move[:1]], self.file_letters[move[3:4]], 8 - int(move[4:5]), 8 - int(move[2:3]), self.file_letters[move[1:2]], 0)
-        elif move == 'resign':
-            return (self.active_color[0], 0, 0, 0, 0, 0, 99)
-        elif move == 'draw':
-            return (self.active_color[0], 0, 0, 0, 0, 0, 98)
-        elif move == 'o-o':
-            return (self.active_color[0], 0, 0, 0, 0, 0, 20)
-        elif move == 'o-o-o':
-            return (self.active_color[0], 0, 0, 0, 0, 0, 30)
-        else:
-            return (0, 0, 0, 0, 0, 0, 97)
 
 
 
